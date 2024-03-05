d8cca38d64307cf558c3fee2479f2ab2e699dacb
Venkata Karthik <99868069+venkatakartrhik@users.noreply.github.com>
2024-03-05 11:30:43 +0530

    #include<stdio.h> int main() {     int p,t,r;     scanf("%d %d %d",&p,&t,&r);     printf("%d",((p*t*r)/100)); }

--- SIMPLE_INTEREST.c
+++ SIMPLE_INTEREST.c
@@ -1,9 +1,7 @@
 #include<stdio.h>
 int main()
 {
-   int p,t,r,simpleinterest;
-   scanf("%d%d%d",&p,&t,&r);
-   simpleinterest=(p*t*r)/100;
-   printf("%d",simpleinterest);
-   return 0;
+    int p,t,r;
+    scanf("%d %d %d",&p,&t,&r);
+    printf("%d",((p*t*r)/100));
 }
