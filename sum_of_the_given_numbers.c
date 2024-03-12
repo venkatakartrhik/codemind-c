895378221b968c4a3cbfbfc55b7849dd3cfbbc6b
Venkata Karthik <99868069+venkatakartrhik@users.noreply.github.com>
2024-03-12 11:12:07 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int i;     for (i=0;i<n;i++)     {         int x,y;         scanf("%d %d",&x,&y);         int z=x+y;         printf("%d ",z);     } }

--- sum_of_the_given_numbers.c
+++ sum_of_the_given_numbers.c
@@ -3,11 +3,13 @@ int main()
 {
     int n;
     scanf("%d",&n);
-    while(n--)
+    int i;
+    for (i=0;i<n;i++)
     {
         int x,y;
         scanf("%d %d",&x,&y);
+        int z=x+y;
         printf("%d
-",(x+y));
+",z);
     }
 }
